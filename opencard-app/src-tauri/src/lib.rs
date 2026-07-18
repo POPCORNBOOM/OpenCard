b8f2c7c14a6cc1abd944f9c84325d7cf0bd70e29
Mihua <2602977906@qq.com>
2026-07-18 23:31:52 +0800

    auto update

--- opencard-app/src-tauri/src/lib.rs
+++ opencard-app/src-tauri/src/lib.rs
@@ -82,6 +82,7 @@ fn stop_watching(state: State<WatcherState>) -> Result<String, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
